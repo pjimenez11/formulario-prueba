1ddb7bdac2b6aa2c16e69800d2083bc028fc62f8
pjimenez11 <pjimenez1025@uta.edu.ec>
2023-12-03 06:08:15 -0500

    Update Form component styling

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -16,7 +16,7 @@ export default function Form() {
           <span>Guardar</span>
         </button>
       </div>
-      <div className="flex items-center justify-around bg-blue-100 dark:bg-gray-700 border border-gray-200 dark:border-gray-700 rounded-md p-2">
+      <div className="flex flex-col md:flex-row items-center justify-center md:justify-around bg-blue-100 dark:bg-gray-700 border border-gray-200 dark:border-gray-700 rounded-md p-2">
         <div className="flex items-center gap-3">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/a/a4/Logotipo_empresa.png"
@@ -27,7 +27,7 @@ export default function Form() {
             Company ABC
           </span>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col items-center">
           <h1 className="text-2xl font-bold text-gray-800 dark:text-gray-100">
             Solicitud de cambio
           </h1>
